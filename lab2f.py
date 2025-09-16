706b589bea3c42cb91166f104abf0eef12b84ed1
Nilkanth Patel <njpatel29@myseneca.ca>
2025-09-16 14:45:10 -0400

    Lab 2 completed and tested

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Nilkanth Jigneshbhai Patel
+# Author ID: njpatel29@myseneca.ca
+# Date Created: 2025/09/16
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
